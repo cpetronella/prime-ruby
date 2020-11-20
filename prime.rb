9bb9f670bf5efb69482c10bd936f5471f2ce2a47
Christopher <christopher.petronella@gmail.com>
2020-11-20 15:28:24 -0500

    commit tree 6171e7453ebff3d216a72ef8af0543376313f54a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(primes)
+  if primes <= 1 || primes == 0 || primes == 1
+    return false
+  elsif
+    (2..primes - 1).each do |name|
+      if primes % name == 0
+      return false
+    end
+  end
+end
+true
+end
+
+
